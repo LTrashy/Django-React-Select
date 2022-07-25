c8a31403c991d32d697083a439b3f4cefaa1ea01
Steven <andersongonzalez2001@gmail.com>
2022-07-24 21:40:09 -0500

    todo: finish the logic to select

--- react-proy/src/components/NavBar.js
+++ react-proy/src/components/NavBar.js
@@ -1,11 +1,12 @@
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          React App - CRUD dep
+          Select App
         </Link>
         <button
           className="navbar-toggler"
@@ -20,18 +21,36 @@ const NavBar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <Link className="nav-link active" aria-current="page" to="/">
+            <Link className="nav-link active me-3" aria-current="page" to="/">
               Home
             </Link>
-            <Link className="nav-link" to="/companyForm">
-              Add Departamento
-            </Link>
-            <Link className="nav-link" to="/companyForm">
-              Add Ciudad
-            </Link>
-            <Link className="nav-link" to="/companyForm">
-              Add Localidad
-            </Link>
+            <DropdownButton
+              id="dropdown-basic"
+              variant="Secondary"
+              title="Departamento"
+            >
+              <Dropdown.Item href="#/action-1">Agragar</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Editar</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Eliminar</Dropdown.Item>
+            </DropdownButton>
+            <DropdownButton
+              id="dropdown-basic"
+              variant="Secondary"
+              title="Ciudad"
+            >
+              <Dropdown.Item href="#/action-1">Agragar</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Editar</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Eliminar</Dropdown.Item>
+            </DropdownButton>
+            <DropdownButton
+              id="dropdown-basic"
+              variant="Secondary"
+              title="Localidad"
+            >
+              <Dropdown.Item href="#/action-1">Agragar</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Editar</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Eliminar</Dropdown.Item>
+            </DropdownButton>
           </div>
         </div>
       </div>
